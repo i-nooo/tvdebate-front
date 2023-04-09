8eb8d63d90c79bf3f2656efb21628b7b6e6784d0
문광혁 <moonkwanghyuk@mungwanghyeog-ui-MacBookAir.local>
2023-04-10 06:10:53 +0900

    0410

--- src/views/ConceptualRecurrencePlot/Drawers/src/redux/actionTypes.ts
+++
@@ -1,6 +0,0 @@
-// export const ADD_TODO = "ADD_TODO";
-// export const TOGGLE_TODO = "TOGGLE_TODO";
-// export const SET_FILTER = "SET_FILTER";
-
-export const CHANGE_STANDARD_SIMILARITY_SCORE =
-  "CHANGE_STANDARD_SIMILARITY_SCORE";
